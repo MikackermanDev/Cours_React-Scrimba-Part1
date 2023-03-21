9139b6b0b57abc3f5d39d4ae5efb6b52dd1dd92b
MikackermanDev <122027129+MikackermanDev@users.noreply.github.com>
2023-03-21 08:43:23 +0100

    save

---
+++ src/index2.js
@@ -0,0 +1,11 @@
+import React from "react";
+import ReactDOM from "react";
+
+const page3 = (
+	<div>
+		<img src="./assets/img/react-logo.png" alt="" />
+		<h1>Encore une page</h1>
+	</div>
+);
+
+const route3 = 
