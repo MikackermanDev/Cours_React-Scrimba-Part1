884046bd978cecca4c77c76c504a74fa09b74805
MikackermanDev <122027129+MikackermanDev@users.noreply.github.com>
2023-03-21 08:43:28 +0100

    save

--- src/index.js
+++ src/index.js
@@ -1,9 +1,28 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
+const page = (
+	<div>
+		<h1>Mon super site</h1>
+		<h3>Pourquoi j'aime React</h3>
+		<ol>
+			<li>J'ai pas trop le choix dans ce tuto </li>
+			<li>C'est des composants</li>
+			<li>C'est déclarative</li>
+			<li>Employabilité élevée</li>
+		</ol>
+	</div>
+);
+
+// 1er écriture
+ReactDOM.render(page, document.getElementById("root2"));
+
+//2nd ecriture
+//document.getElementById("root").append(JSON.stringify(page));
+
 const navbar = (
 	<nav>
-		<h2>GPU DataBase</h2>
+		<h3>GPU DataBase</h3>
 		<ul>
 			<li>4090</li>
 			<li>4080</li>
@@ -12,7 +31,9 @@ const navbar = (
 	</nav>
 );
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(navbar);
+// 1ere écriture
+const route = ReactDOM.createRoot(document.getElementById("root1"));
+route.render(navbar);
 
+// 2nd écriture
 // ReactDOM.createRoot(document.getElementById("root")).render(navbar);
