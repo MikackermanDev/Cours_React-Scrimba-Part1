ad2cb0db59b10a868ff86ed3d5d54b61ec1681ca
MikackermanDev <122027129+MikackermanDev@users.noreply.github.com>
2023-03-27 21:07:14 +0200

    save

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -2,19 +2,23 @@ import "./Header.scss";
 
 export default function Header() {
 	return (
-		<div className="nav-link">
-			<img src="src/assets/img/favM.ico" alt="favicon" />
-			<ul>
-				<li>
-					<a href="">Home</a>
-				</li>
-				<li>
-					<a href="">About Me</a>
-				</li>
-				<li>
-					<a href="">Contact Me</a>
-				</li>
-			</ul>
+		<div>
+			<div className="navbar">
+				<img src="src/assets/img/favM.ico" alt="favicon" />
+				<ul>
+					<li>
+						<a href="https://www.google.fr" target="blank">
+							Home
+						</a>
+					</li>
+					<li>
+						<a href="">About Me</a>
+					</li>
+					<li>
+						<a href="">Contact Me</a>
+					</li>
+				</ul>
+			</div>
 		</div>
 	);
 }
